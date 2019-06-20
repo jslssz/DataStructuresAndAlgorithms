7b7ca31686fcb74c6eed0187cb6b1ea1770519dc
jxustcmh <jxrjcmh@163.com>
2019-06-20 12:15:23 +0800

    add

---
+++ DataStructures/src/com/hx/recursive/hsp/RecursiveDemo.java
@@ -0,0 +1,19 @@
+package com.hx.recursive.hsp;
+
+/**
+ * @author jxlgcmh
+ * @create 2019-06-19 15:49
+ */
+public class RecursiveDemo {
+	public static void main(String[] args) {
+		test(4);
+	}
+	
+	public static void test(int n) {
+		if (n > 2) {
+			test(n-1);
+		}
+		System.out.println(n);
+		
+	}
+}
