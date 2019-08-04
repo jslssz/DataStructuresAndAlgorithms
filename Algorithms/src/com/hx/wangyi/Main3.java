a745c0a9824842ae2649c3ec1d824479be3198c8
jxustcmh <jxrjcmh@163.com>
2019-08-04 09:16:52 +0800

    网易的三道题

---
+++ Algorithms/src/com/hx/wangyi/Main3.java
@@ -0,0 +1,20 @@
+package com.hx.wangyi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author jxlgcmh
+ * @date 2019-08-03 16:25
+ */
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr =new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]= sc.nextInt();
+        }
+    }
+}
